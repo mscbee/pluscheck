fece358b51a5a7959ff37f01a405854b2516f1e1
mscbee <charlenebadibanga@gmail.com>
2019-06-24 21:56:38 +0100

    added Heading component for logo

---
+++ Heading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Heading extends Component {
+    render() {
+        return (
+            <View>
+                <Text>+check</Text>
+            </View>
+        );
+    }
+}
+
+export default Heading;
